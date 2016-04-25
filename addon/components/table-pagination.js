0597bbc48e2146cad1fb8293fedc7b1aecf956f3
leojpod <leo.jpod@gmail.com>
2016-04-25 23:51:31 +0200

    unpod the component

--- addon/components/table-pagination.js
+++ addon/components/table-pagination.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import layout from '../templates/table-pagination';
+import layout from '../templates/components/table-pagination';
 
 export default Ember.Component.extend({
   layout
